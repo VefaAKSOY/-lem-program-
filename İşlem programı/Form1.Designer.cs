316b5f778eabd09462c7503652f142a448d08b32
Bayram Vefa AKSOY <bayramvefa.aksoy@bakircay.edu.tr>
2019-10-04 23:03:28 +0300

    Proje dosyası ekle.

---
+++ İşlem programı/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace İşlem_programı
+{
+    partial class frmHesaplama
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHesaplama));
+            this.tbcIşlemler = new System.Windows.Forms.TabControl();
+            this.tbpTekSayi = new System.Windows.Forms.TabPage();
+            this.lsvTekSayilar = new System.Windows.Forms.ListView();
+            this.tbpCiftSayi = new System.Windows.Forms.TabPage();
+            this.lsvCiftSayilar = new System.Windows.Forms.ListView();
+            this.tbpAsalSayilar = new System.Windows.Forms.TabPage();
+            this.lsvAsalSayilar = new System.Windows.Forms.ListView();
+            this.tbpArmstrongSayilari = new System.Windows.Forms.TabPage();
+            this.lsvArmstrongSayilari = new System.Windows.Forms.ListView();
+            this.grpSayiEkrani = new System.Windows.Forms.GroupBox();
+            this.mtbIkinciSayi = new System.Windows.Forms.MaskedTextBox();
+            this.mtbIlkSayi = new System.Windows.Forms.MaskedTextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblIkinciSayi = new System.Windows.Forms.Label();
+            this.lblIlkSayi = new System.Windows.Forms.Label();
+            this.btnGeri = new System.Windows.Forms.Button();
+            this.btnİleri = new System.Windows.Forms.Button();
+            this.ntfBildirim = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tbcIşlemler.SuspendLayout();
+            this.tbpTekSayi.SuspendLayout();
+            this.tbpCiftSayi.SuspendLayout();
+            this.tbpAsalSayilar.SuspendLayout();
+            this.tbpArmstrongSayilari.SuspendLayout();
+            this.grpSayiEkrani.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbcIşlemler
+            // 
+            this.tbcIşlemler.Controls.Add(this.tbpTekSayi);
+            this.tbcIşlemler.Controls.Add(this.tbpCiftSayi);
+            this.tbcIşlemler.Controls.Add(this.tbpAsalSayilar);
+            this.tbcIşlemler.Controls.Add(this.tbpArmstrongSayilari);
+            this.tbcIşlemler.Location = new System.Drawing.Point(16, 101);
+            this.tbcIşlemler.Name = "tbcIşlemler";
+            this.tbcIşlemler.SelectedIndex = 0;
+            this.tbcIşlemler.Size = new System.Drawing.Size(641, 322);
+            this.tbcIşlemler.TabIndex = 5;
+            // 
+            // tbpTekSayi
+            // 
+            this.tbpTekSayi.Controls.Add(this.lsvTekSayilar);
+            this.tbpTekSayi.Location = new System.Drawing.Point(4, 25);
+            this.tbpTekSayi.Name = "tbpTekSayi";
+            this.tbpTekSayi.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpTekSayi.Size = new System.Drawing.Size(633, 293);
+            this.tbpTekSayi.TabIndex = 0;
+            this.tbpTekSayi.Text = "Tek Sayılar";
+            this.tbpTekSayi.UseVisualStyleBackColor = true;
+            // 
+            // lsvTekSayilar
+            // 
+            this.lsvTekSayilar.HideSelection = false;
+            this.lsvTekSayilar.Location = new System.Drawing.Point(3, 0);
+            this.lsvTekSayilar.Name = "lsvTekSayilar";
+            this.lsvTekSayilar.Size = new System.Drawing.Size(624, 290);
+            this.lsvTekSayilar.TabIndex = 0;
+            this.lsvTekSayilar.UseCompatibleStateImageBehavior = false;
+            this.lsvTekSayilar.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // tbpCiftSayi
+            // 
+            this.tbpCiftSayi.Controls.Add(this.lsvCiftSayilar);
+            this.tbpCiftSayi.Location = new System.Drawing.Point(4, 25);
+            this.tbpCiftSayi.Name = "tbpCiftSayi";
+            this.tbpCiftSayi.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpCiftSayi.Size = new System.Drawing.Size(633, 293);
+            this.tbpCiftSayi.TabIndex = 1;
+            this.tbpCiftSayi.Text = "Çift Sayılar";
+            this.tbpCiftSayi.UseVisualStyleBackColor = true;
+            // 
+            // lsvCiftSayilar
+            // 
+            this.lsvCiftSayilar.HideSelection = false;
+            this.lsvCiftSayilar.Location = new System.Drawing.Point(0, 6);
+            this.lsvCiftSayilar.Name = "lsvCiftSayilar";
+            this.lsvCiftSayilar.Size = new System.Drawing.Size(627, 284);
+            this.lsvCiftSayilar.TabIndex = 0;
+            this.lsvCiftSayilar.UseCompatibleStateImageBehavior = false;
+            this.lsvCiftSayilar.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // tbpAsalSayilar
+            // 
+            this.tbpAsalSayilar.Controls.Add(this.lsvAsalSayilar);
+            this.tbpAsalSayilar.Location = new System.Drawing.Point(4, 25);
+            this.tbpAsalSayilar.Name = "tbpAsalSayilar";
+            this.tbpAsalSayilar.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpAsalSayilar.Size = new System.Drawing.Size(633, 293);
+            this.tbpAsalSayilar.TabIndex = 2;
+            this.tbpAsalSayilar.Text = "Asal Sayılar";
+            this.tbpAsalSayilar.UseVisualStyleBackColor = true;
+            // 
+            // lsvAsalSayilar
+            // 
+            this.lsvAsalSayilar.HideSelection = false;
+            this.lsvAsalSayilar.Location = new System.Drawing.Point(6, 6);
+            this.lsvAsalSayilar.Name = "lsvAsalSayilar";
+            this.lsvAsalSayilar.Size = new System.Drawing.Size(621, 284);
+            this.lsvAsalSayilar.TabIndex = 0;
+            this.lsvAsalSayilar.UseCompatibleStateImageBehavior = false;
+            this.lsvAsalSayilar.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // tbpArmstrongSayilari
+            // 
+            this.tbpArmstrongSayilari.Controls.Add(this.lsvArmstrongSayilari);
+            this.tbpArmstrongSayilari.Location = new System.Drawing.Point(4, 25);
+            this.tbpArmstrongSayilari.Name = "tbpArmstrongSayilari";
+            this.tbpArmstrongSayilari.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpArmstrongSayilari.Size = new System.Drawing.Size(633, 293);
+            this.tbpArmstrongSayilari.TabIndex = 3;
+            this.tbpArmstrongSayilari.Text = "Armstrong Sayıları";
+            this.tbpArmstrongSayilari.UseVisualStyleBackColor = true;
+            // 
+            // lsvArmstrongSayilari
+            // 
+            this.lsvArmstrongSayilari.HideSelection = false;
+            this.lsvArmstrongSayilari.Location = new System.Drawing.Point(6, 6);
+            this.lsvArmstrongSayilari.Name = "lsvArmstrongSayilari";
+            this.lsvArmstrongSayilari.Size = new System.Drawing.Size(624, 281);
+            this.lsvArmstrongSayilari.TabIndex = 0;
+            this.lsvArmstrongSayilari.UseCompatibleStateImageBehavior = false;
+            this.lsvArmstrongSayilari.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // grpSayiEkrani
+            // 
+            this.grpSayiEkrani.Controls.Add(this.btnGeri);
+            this.grpSayiEkrani.Controls.Add(this.btnİleri);
+            this.grpSayiEkrani.Controls.Add(this.mtbIkinciSayi);
+            this.grpSayiEkrani.Controls.Add(this.mtbIlkSayi);
+            this.grpSayiEkrani.Controls.Add(this.btnHesapla);
+            this.grpSayiEkrani.Controls.Add(this.lblIkinciSayi);
+            this.grpSayiEkrani.Controls.Add(this.lblIlkSayi);
+            this.grpSayiEkrani.Location = new System.Drawing.Point(12, 12);
+            this.grpSayiEkrani.Name = "grpSayiEkrani";
+            this.grpSayiEkrani.Size = new System.Drawing.Size(645, 83);
+            this.grpSayiEkrani.TabIndex = 5;
+            this.grpSayiEkrani.TabStop = false;
+            this.grpSayiEkrani.Text = "Sayı Girişi";
+            // 
+            // mtbIkinciSayi
+            // 
+            this.mtbIkinciSayi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mtbIkinciSayi.Location = new System.Drawing.Point(77, 54);
+            this.mtbIkinciSayi.Mask = "00000";
+            this.mtbIkinciSayi.Name = "mtbIkinciSayi";
+            this.mtbIkinciSayi.Size = new System.Drawing.Size(100, 15);
+            this.mtbIkinciSayi.TabIndex = 7;
+            this.mtbIkinciSayi.Tag = "";
+            this.mtbIkinciSayi.Text = "10000";
+            this.mtbIkinciSayi.ValidatingType = typeof(int);
+            // 
+            // mtbIlkSayi
+            // 
+            this.mtbIlkSayi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mtbIlkSayi.Location = new System.Drawing.Point(77, 25);
+            this.mtbIlkSayi.Mask = "0000";
+            this.mtbIlkSayi.Name = "mtbIlkSayi";
+            this.mtbIlkSayi.Size = new System.Drawing.Size(100, 15);
+            this.mtbIlkSayi.TabIndex = 6;
+            this.mtbIlkSayi.Tag = "";
+            this.mtbIlkSayi.Text = "1";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(199, 28);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 23);
+            this.btnHesapla.TabIndex = 2;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblIkinciSayi
+            // 
+            this.lblIkinciSayi.AutoSize = true;
+            this.lblIkinciSayi.Location = new System.Drawing.Point(1, 56);
+            this.lblIkinciSayi.Name = "lblIkinciSayi";
+            this.lblIkinciSayi.Size = new System.Drawing.Size(70, 17);
+            this.lblIkinciSayi.TabIndex = 4;
+            this.lblIkinciSayi.Text = "İkinci Sayi";
+            // 
+            // lblIlkSayi
+            // 
+            this.lblIlkSayi.AutoSize = true;
+            this.lblIlkSayi.Location = new System.Drawing.Point(1, 28);
+            this.lblIlkSayi.Name = "lblIlkSayi";
+            this.lblIlkSayi.Size = new System.Drawing.Size(52, 17);
+            this.lblIlkSayi.TabIndex = 1;
+            this.lblIlkSayi.Text = "İlk Sayi";
+            // 
+            // btnGeri
+            // 
+            this.btnGeri.Location = new System.Drawing.Point(382, 28);
+            this.btnGeri.Name = "btnGeri";
+            this.btnGeri.Size = new System.Drawing.Size(79, 23);
+            this.btnGeri.TabIndex = 8;
+            this.btnGeri.Text = "<<Geri";
+            this.btnGeri.UseVisualStyleBackColor = true;
+            this.btnGeri.Click += new System.EventHandler(this.btnGeri_Click);
+            // 
+            // btnİleri
+            // 
+            this.btnİleri.Location = new System.Drawing.Point(482, 28);
+            this.btnİleri.Name = "btnİleri";
+            this.btnİleri.Size = new System.Drawing.Size(79, 23);
+            this.btnİleri.TabIndex = 9;
+            this.btnİleri.Text = "İleri>>";
+            this.btnİleri.UseVisualStyleBackColor = true;
+            this.btnİleri.Click += new System.EventHandler(this.btnİleri_Click);
+            // 
+            // ntfBildirim
+            // 
+            this.ntfBildirim.BalloonTipText = "Uygulamaya Hoşgeldiniz";
+            this.ntfBildirim.BalloonTipTitle = "Bakırçay Üniversitesi Ödev";
+            this.ntfBildirim.Icon = ((System.Drawing.Icon)(resources.GetObject("ntfBildirim.Icon")));
+            this.ntfBildirim.Text = "Uygulamaya Hoşgeldiniz";
+            this.ntfBildirim.Visible = true;
+            // 
+            // frmHesaplama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 435);
+            this.Controls.Add(this.grpSayiEkrani);
+            this.Controls.Add(this.tbcIşlemler);
+            this.Name = "frmHesaplama";
+            this.Text = "Hesaplama Programı";
+            this.Shown += new System.EventHandler(this.frmHesaplama_Shown);
+            this.tbcIşlemler.ResumeLayout(false);
+            this.tbpTekSayi.ResumeLayout(false);
+            this.tbpCiftSayi.ResumeLayout(false);
+            this.tbpAsalSayilar.ResumeLayout(false);
+            this.tbpArmstrongSayilari.ResumeLayout(false);
+            this.grpSayiEkrani.ResumeLayout(false);
+            this.grpSayiEkrani.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tbcIşlemler;
+        private System.Windows.Forms.TabPage tbpCiftSayi;
+        private System.Windows.Forms.TabPage tbpTekSayi;
+        private System.Windows.Forms.GroupBox grpSayiEkrani;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblIkinciSayi;
+        private System.Windows.Forms.Label lblIlkSayi;
+        private System.Windows.Forms.ListView lsvCiftSayilar;
+        private System.Windows.Forms.ListView lsvTekSayilar;
+        private System.Windows.Forms.TabPage tbpAsalSayilar;
+        private System.Windows.Forms.TabPage tbpArmstrongSayilari;
+        private System.Windows.Forms.ListView lsvAsalSayilar;
+        private System.Windows.Forms.ListView lsvArmstrongSayilari;
+        private System.Windows.Forms.MaskedTextBox mtbIkinciSayi;
+        private System.Windows.Forms.MaskedTextBox mtbIlkSayi;
+        private System.Windows.Forms.Button btnGeri;
+        private System.Windows.Forms.Button btnİleri;
+        private System.Windows.Forms.NotifyIcon ntfBildirim;
+    }
+}
+
---
+++ İşlem programı/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace İşlem_programı
+{
+    public partial class frmHesaplama : Form
+    {
+     
+        public frmHesaplama()
+        {
+            InitializeComponent();
+        }
+        enum islemTuru
+        {
+            eCiftToplama,
+            eTekToplama,
+            eTekCarpma,
+            eCiftCarpma
+        }
+        string armstrongSayılar (int birinciSayi, int ikinciSayi)
+        {
+            int armstrongSayilariToplam = 0; 
+            BigInteger armstrongSayilaricarpim = 1;
+            string armstrongsayilar = " ";
+            if (birinciSayi == 1) { birinciSayi += 1; }
+            for (int a = birinciSayi; a <= ikinciSayi; a++)
+            {
+                int birlerBasamagi = a % 10;
+                int onlarBasamagi = ((a - birlerBasamagi) / 10) % 10;
+                int yuzlerBasamagi = (((a - birlerBasamagi) / 10) - onlarBasamagi) / 10;
+                if (a == Math.Pow(birlerBasamagi, 3) + Math.Pow(onlarBasamagi, 3) + Math.Pow(yuzlerBasamagi, 3))
+                {
+                    armstrongSayilaricarpim *= a;
+                    armstrongSayilariToplam += a;
+                    lsvArmstrongSayilari.Items.Add(a.ToString());
+                    armstrongsayilar+= a + " ";
+                }    
+            }
+            lsvArmstrongSayilari.Items.Add("Armstrong sayilari toplamı: " + armstrongSayilariToplam.ToString() + "Armstrong Sayıların Çarpımı: " + armstrongSayilaricarpim.ToString());
+            return armstrongsayilar;
+        }
+        string asalSayilar(int ilkSayi,int ikinciSayi)
+        {
+            int asalSayilarToplam=0;
+            string asalsayilar = " ";
+            BigInteger asalSayilarCarpim=1;
+            if (ilkSayi == 1)
+            {
+                ilkSayi++;
+            }
+            for(int i = ilkSayi; i <= ikinciSayi; i++)
+            {
+                int bayrak = 0;
+                for(int j=2;j<i;j++)
+                {
+                    if (i % j == 0)
+                    {
+                        bayrak = 1;
+                        
+                    }
+                }
+                if (bayrak == 0)
+                {
+                    asalSayilarCarpim *= i;
+                    asalSayilarToplam += i;
+                    lsvAsalSayilar.Items.Add(i.ToString());
+                    asalsayilar += i + " ";
+                }
+            }
+            lsvAsalSayilar.Items.Add("Asal Sayılar Toplam: " + asalSayilarToplam.ToString() + "Asal Sayılar Çarpım: " + asalSayilarCarpim.ToString());
+            return asalsayilar;
+        }
+        string ciftYazma(int ilkSayi,int ikinciSayi)
+        {
+            string ciftSayilar = " ";
+            for (int i = ilkSayi; i <= ikinciSayi; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    lsvCiftSayilar.Items.Add(i.ToString());
+                    ciftSayilar += i + " ";
+                }               
+            }
+            return ciftSayilar;
+        }
+        string tekYazma(int sayi1,int sayi2)
+        {
+            string tekSayilar = "";
+            for (int i = sayi1; i <= sayi2; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    lsvTekSayilar.Items.Add(i.ToString());
+                    tekSayilar += i + " ";
+                }
+            }
+            return tekSayilar;
+        }
+        BigInteger islem(islemTuru tur, int ilkSayi, int ikinciSayi)
+        {
+            int ciftToplam = 0, tekToplam = 0;
+            BigInteger ciftCarpim = 1, tekCarpim = 1;                       
+            for (int i = ilkSayi; i <= ikinciSayi; i++)
+            {
+                if (i % 2 == 0)
+                {                                                         
+                    ciftToplam += i;
+                    ciftCarpim *= i;
+                }
+                else
+                {                                 
+                    tekToplam += i;
+                    tekCarpim *= i;
+                }
+            }
+            if (tur == islemTuru.eCiftToplama)
+            {
+                return ciftToplam;
+            }
+            else if (tur == islemTuru.eTekToplama)
+            {
+                return tekToplam;
+            }
+            else if (tur== islemTuru.eCiftCarpma)
+            {
+                return ciftCarpim;
+            }
+            else 
+            {
+                return tekCarpim;
+            }
+        }
+        void Loglama()
+        {
+            StreamWriter Loglama = new StreamWriter("D:\\Log.txt");
+            int baslangic = Convert.ToInt32(mtbIlkSayi.Text);
+            int bitis = Convert.ToInt32(mtbIkinciSayi.Text);
+            if (tbcIşlemler.SelectedIndex == 0)
+            {
+                Loglama.WriteLine("Tek Sayılar:\n" 
+                    + tekYazma(baslangic, bitis) 
+                    +("Tek sayı Toplama: "+islem(islemTuru.eTekToplama, baslangic, bitis).ToString()) 
+                    +("Tek sayı çarpma: " + islem(islemTuru.eTekCarpma, baslangic, bitis).ToString()));
+                Loglama.Close();
+            }
+            else if (tbcIşlemler.SelectedIndex == 1)
+            {
+                Loglama.WriteLine("Tek Sayılar:\n" + ciftYazma(baslangic, bitis) +
+                lsvCiftSayilar.Items.Add("Çift Sayı Toplama: " + islem(islemTuru.eCiftToplama, baslangic, bitis).ToString()) +
+                lsvCiftSayilar.Items.Add("çift sayı çarpma: " + islem(islemTuru.eCiftCarpma, baslangic, bitis).ToString()));
+                Loglama.Close();
+            }
+            else if (tbcIşlemler.SelectedIndex == 2)
+            {
+                Loglama.WriteLine("Asal Sayılar: " + asalSayilar(baslangic,bitis));
+                Loglama.Close();
+            }
+            else if (tbcIşlemler.SelectedIndex == 3)
+            {
+                Loglama.WriteLine("Armstrong sayılar:" + armstrongSayılar(baslangic, bitis));
+                Loglama.Close();
+            }
+        }
+        void EkranHazirla()
+        {
+            mtbIlkSayi.Focus();
+            lsvArmstrongSayilari.Clear();
+            lsvAsalSayilar.Clear();
+            lsvCiftSayilar.Clear();
+            lsvTekSayilar.Clear();
+        }
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int baslangic = Convert.ToInt32(mtbIlkSayi.Text);
+            int bitis = Convert.ToInt32(mtbIkinciSayi.Text);           
+            if (tbcIşlemler.SelectedIndex == 0)
+            {
+                EkranHazirla();
+                lsvTekSayilar.Items.Add("Tek sayı Toplama: "+islem(islemTuru.eTekToplama, baslangic, bitis).ToString());
+                lsvTekSayilar.Items.Add("Tek sayı çarpma: "+islem(islemTuru.eTekCarpma, baslangic, bitis).ToString());
+                tekYazma(baslangic, bitis);
+                Loglama(); 
+            }
+            else if (tbcIşlemler.SelectedIndex == 1)
+            {
+                EkranHazirla();
+                lsvCiftSayilar.Items.Add("Çift Sayı Toplama: " + islem(islemTuru.eCiftToplama, baslangic, bitis).ToString());
+                lsvCiftSayilar.Items.Add("çift sayı çarpma: "+islem(islemTuru.eCiftCarpma, baslangic, bitis).ToString());
+                ciftYazma(baslangic, bitis);
+                Loglama();
+            }
+            else if (tbcIşlemler.SelectedIndex == 2)
+            {
+                EkranHazirla();
+                asalSayilar(baslangic, bitis);
+                Loglama();
+            }
+            else if (tbcIşlemler.SelectedIndex == 3)
+            {
+                EkranHazirla();
+                armstrongSayılar(baslangic, bitis);
+                Loglama();
+            }            
+        }
+        private void frmHesaplama_Shown(object sender, EventArgs e)
+        {
+            EkranHazirla();
+            ntfBildirim.ShowBalloonTip(5000);
+        }
+        private void btnGeri_Click(object sender, EventArgs e)
+        {
+            if (tbcIşlemler.SelectedIndex == 0)
+            {
+                tbcIşlemler.SelectedIndex = 0;
+            }
+            else
+            tbcIşlemler.SelectedIndex -= 1;
+        }
+        private void btnİleri_Click(object sender, EventArgs e)
+        {
+            if (tbcIşlemler.SelectedIndex == 3)
+            {
+                tbcIşlemler.SelectedIndex = 3;
+            }
+            else
+                tbcIşlemler.SelectedIndex += 1;
+        }
+    }
+}
